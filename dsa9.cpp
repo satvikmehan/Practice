d03914a6b53bdc9eef9c2aa696307709b514dba8
satvikmehan <satvikmehan@gmail.com>
2026-01-12 17:18:25 +0530

    Progress

--- dsa9.cpp
+++ dsa9.cpp
@@ -71,13 +71,43 @@ vector<int> productExceptSelf2(vector<int> &nums)
     return ans;
 }
 
-vector<int> productExceptSelf(vector<int> &nums)
+vector<int> productExceptSelf0(vector<int> &nums)
 {
     int n = nums.size();
     vector<int> ans;
-    for (int i = 0; i < n; i++)
+    vector<int> suffix(n,1);
+    vector<int> prefix(n,1);
+    prefix[0]=1;
+    for(int i=1;i<n;i++)
     {
-        
+        prefix[i]=prefix[i-1]*nums[i-1];
+    }
+    suffix[n-1]=1;
+    for(int i=n-2;i>=0;i--)
+    {
+        suffix[i]=suffix[i+1]*nums[i+1];
+    }
+    for(int i=0;i<n;i++)
+    {
+        ans.push_back(prefix[i]*suffix[i]);
+    }
+    return ans;
+}
+
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans(n,1);
+    for(int i=1;i<n;i++)
+    {
+        ans[i]=ans[i-1]*nums[i-1];
+    }
+
+    int suffix=1;
+    for(int i=n-2;i>=0;i--)
+    {
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
     }
     return ans;
 }
